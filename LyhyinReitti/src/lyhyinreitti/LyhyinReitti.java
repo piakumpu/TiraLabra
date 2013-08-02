f22369a43a679e5c61165a04c28eaee9a7a81b1e
Pia Kumpulainen <pia.kumpulainen@cs.helsinki.fi>
2013-08-02 09:39:30 +0300

    Projekti luotu

---
+++ LyhyinReitti/src/lyhyinreitti/LyhyinReitti.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lyhyinreitti;
+
+/**
+ *
+ * @author piakumpu
+ */
+public class LyhyinReitti {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
